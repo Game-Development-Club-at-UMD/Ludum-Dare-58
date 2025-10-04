fe83fb0f99c6f421dcef87e9489c4750aa51bbf0
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2025-10-04 00:49:34 -0500

    Implemented button function
    Needs to be hooked up to dodging functionality when it gets created

---
+++ ludum-dare-58/Buttons/DodgeButton.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var label: Label = $Label
+@onready var cooldown_timer: Timer = $CooldownTimer
+@onready var dodge_timer: Timer = $DodgeTimer
+
+
+signal dodgingStart
+signal dodgingEnd
+
+
+func _ready() -> void:
+	pass #connect signal to cooldownTimer wait time
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Dodge") and cooldown_timer.is_stopped():
+		label.add_theme_color_override("font_color", Color(1, 1, 1, 0.5))
+		startDodge()
+		
+
+
+func startDodge():
+	dodgingStart.emit()
+	dodge_timer.start()
+	cooldown_timer.start()
+	await dodge_timer.timeout
+	dodgingEnd.emit()
+	await cooldown_timer.timeout
+	
+	label.add_theme_color_override("font_color", Color(1, 1, 1, 1))
+
+
+func updateCooldownTimer(cooldown : float, dodgeLength : float):
+	dodge_timer.wait_time = dodgeLength
+	cooldown_timer.wait_time = cooldown - dodgeLength
