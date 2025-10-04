b2a3636eb8887d4aa47f137ce89f2ea945ff3526
Athish <Athish1123@gmail.com>
2025-10-04 00:17:13 -0500

    Added Modifiers

--- ludum-dare-58/Moves/AttackMove.gd
+++ ludum-dare-58/Moves/AttackMove.gd
@@ -2,5 +2,13 @@ class_name AttackMove
 extends Move
 
 @export var sprite : Texture2D
-@export var Damage_Value : int = 0
-@export var modifier : Modifier
+@export var Damage_Value : int = 2
+@export_enum("DamageAtMaxHealth", "LowDamage") var modifier_type : int
+@export var MaxHp : int
+
+var modifier : Modifier = Modifier.new()
+
+func DoAttack():
+	print(modifier_type)
+	Damage_Value = modifier.apply_modifier(modifier_type, Damage_Value, MaxHp)
+	return Damage_Value
