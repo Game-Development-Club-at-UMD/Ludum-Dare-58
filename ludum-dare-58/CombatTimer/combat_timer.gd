24dbcc0424bf2fb70c0d25fd844c6b34a54c8d4a
Athish <Athish1123@gmail.com>
2025-10-04 16:23:15 -0500

    Made a test scene to see if button works with timer

--- ludum-dare-58/CombatTimer/combat_timer.gd
+++ ludum-dare-58/CombatTimer/combat_timer.gd
@@ -4,20 +4,23 @@ extends Node2D
 @onready var progressbar = $ProgressBar
 @onready var button = $Button
 
+func buttonTimer() -> void:
+	if(timer.time_left > 0):
+		button.disabled = false
+	else:
+		button.disabled = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.wait_time = progressbar.max_value
 	timer.start()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:) -> void:
-	if(timer.time_left > 0):
-		button.disabled = true
-	else:
-		button.disabled = false
+	buttonTimer()
 	progressbar.value = timer.time_left		
+	
 
 
-func _on_button_button_down() -> void:
+func _on_button_pressed() -> void:
 	print("yay")
