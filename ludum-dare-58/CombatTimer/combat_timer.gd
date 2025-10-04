bb9db5f145671acdfe2c160b4da7738edb90dcb5
Athish <Athish1123@gmail.com>
2025-10-04 01:49:56 -0500

    Added a tester button that correlates to the timer

--- ludum-dare-58/CombatTimer/combat_timer.gd
+++ ludum-dare-58/CombatTimer/combat_timer.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var timer = $Timer
 @onready var progressbar = $ProgressBar
-
+@onready var button = $Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +12,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:) -> void:
+	if(timer.time_left > 0):
+		button.disabled = true
+	else:
+		button.disabled = false
 	progressbar.value = timer.time_left		
+
+
+func _on_button_button_down() -> void:
+	print("yay")
