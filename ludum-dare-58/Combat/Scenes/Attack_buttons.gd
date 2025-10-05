3b747ef7419de9c8ebb2618e78c6d87b9137f593
Athish <Athish1123@gmail.com>
2025-10-05 18:04:48 -0500

    FUCKIGN FIXED THIS SHITTY ASS BUTTONS (FUCK ME)

--- ludum-dare-58/Combat/Scenes/Attack_buttons.gd
+++ ludum-dare-58/Combat/Scenes/Attack_buttons.gd
@@ -27,10 +27,14 @@ func attackMove(newMoveHolder :MoveHolder):
 func debug():
 	var move1 : AttackMove = load("res://Moves/Resources/BasicAttack.tres")
 	var move2 : AttackMove = load("res://Moves/Resources/BasicAttack2.tres")
+	var move3 : AttackMove = load("res://Moves/Resources/BasicAttack.tres")
+	var move4 : AttackMove = load("res://Moves/Resources/BasicAttack.tres")
 
 	var h : MoveHolder = MoveHolder.new()
 	h.addtoMoveDict("Move1", move1)
 	h.addtoMoveDict("Move2", move2)
+	h.addtoMoveDict("Move3", move1)
+	h.addtoMoveDict("Move4", move1)
 	
 	moveHolderInstance = h
 	print(moveHolderInstance.moveDict)
