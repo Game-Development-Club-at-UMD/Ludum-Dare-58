8f2c9670ed6c31e3010d7a737bc2bb11bf3bd0eb
Carter JJ <jorgensencarter@gmail.com>
2025-10-05 14:32:17 -0500

    fixed fullscreen button
    fixed da button

--- ludum-dare-58/Main Menu/main_menu.gd
+++ ludum-dare-58/Main Menu/main_menu.gd
@@ -51,7 +51,7 @@ func _on_back_pressed() -> void:
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
